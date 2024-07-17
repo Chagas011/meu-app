412a343cb50361753dce945ff6a26fb05fdadb28
Chagas011 <washington.chagas.9@hotmail.com>
2024-07-17 08:10:31 -0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'meu-app';
